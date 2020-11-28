669caaa431af2d4cdbb7cc36611148955c669140
Brett Mayson <brett@mayson.io>
2020-11-28 19:34:09 +0000

    some tweaks

--- docs/js/members.js
+++ docs/js/members.js
@@ -1,37 +1,49 @@
-roles = [[700888852142751815, "staff"], [700892038668877844, "senior"], [700892097775009842, "juniors"], [700954650026704906, "members"]]
+roles = {
+  staff: 700888852142751815,
+  senior: 700892038668877844,
+  members: 700954650026704906,
+  juniors: 700892097775009842,
+};
 certsList = [[780136967677411389, "Medic"], [780138101834121236, "Grenadier"], [780136990405296128, "EOD"], [780137042180046848, "Autorifleman"], [780137008411705375, "Marksman"]]
 
 function list(x){
-  for (i=0; i<x.length; i++){
-    if(x[i].nick){
-      var iDiv = document.createElement('div');
-      iDiv.id = 'member';
+  x.forEach((user) => {
+    console.log(user);
+    let nick = user.nick || user.user.name;
+    var iDiv = document.createElement('div');
 
-      if(x[i].roles[0] == 700888852142751815){iDiv.className = 'col-md-2 single-team';}
-      else{iDiv.className = 'col-md-3 single-team';}
+    if (user.roles.includes(roles.staff)) {
+      iDiv.className = 'col-md-2 single-team';
+    } else {
+      iDiv.className = 'col-md-3 single-team';
+    }
 
-      var textContainer = document.createElement('div');
-      textContainer.className = 'meta-text mt-30 text-center';
-      iDiv.appendChild(textContainer);
+    var textContainer = document.createElement('div');
+    textContainer.className = 'meta-text mt-30 text-center';
+    iDiv.appendChild(textContainer);
 
-      textContainer.innerHTML = '<h4>' + x[i].nick +  '</h4>';
-      var certs = [];
-      for(l = 0; l < certsList.length; l++){
-        for(z = 0; z < x[i].roles.length; z++){
-          if (x[i].roles[z] == certsList[l][0]){
+    textContainer.innerHTML = '<h4>' + nick +  '</h4>';
+
+    var certs = [];
+    if (!user.roles.includes(roles.staff)) {
+      for (l = 0; l < certsList.length; l++) {
+        for (z = 0; z < user.roles.length; z++) {
+          if (user.roles[z] == certsList[l][0]) {
             certs.push(certsList[l][1]);
-          };
-        };
-      };
+          }
+        }
+      }
+    }
 
-      textContainer.innerHTML += "<p>" + certs.join(" | ") + "</p>";
+    textContainer.innerHTML += "<p>" + certs.join(" | ") + "</p>";
 
-      for (j = 0; j < roles.length; j++){
-        if (x[i].roles[0] == roles[j][0]){document.getElementById(roles[j][1]).appendChild(iDiv)};
-      };
-    };
-  };
-};
+    Object.keys(roles).forEach((role) => {
+      if (user.roles[0] == roles[role]) {
+        document.getElementById(role).appendChild(iDiv);
+      }
+    });
+  });
+}
 fetch('https://cors-anywhere.herokuapp.com/https://dev.dynulo.com/v1/bot/discord/guild/700888247928356905/members')
 .then(response => response.json())
 .then(data => list(data));
